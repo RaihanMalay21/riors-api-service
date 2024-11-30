2c31a04e9a7c0169700c940cc59ac1162724580c
RaihanMalay21 <raihanmalay14@gmail.com>
2024-11-30 07:35:32 -0800

    delete path prefix end point api

--- router/router.go
+++ router/router.go
@@ -9,13 +9,12 @@ import (
 )
 
 func InitRouter(e *echo.Echo, product controller.ProductController, category controller.CategoryController) {
-	
+
 	e.Use(middlewares.CorsMiddlewares)
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
-	g := e.Group("/riors")
-	g.GET("/product", product.GetAllProduct)
-	g.POST("/product/input", product.InputProduct)
-	g.GET("/category", category.GetAllCategory)
-	g.POST("/category/input", category.InputCategory)
+	e.GET("/product", product.GetAllProduct)
+	e.POST("/product/input", product.InputProduct)
+	e.GET("/category", category.GetAllCategory)
+	e.POST("/category/input", category.InputCategory)
 
 }

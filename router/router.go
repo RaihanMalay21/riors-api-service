bf66491f04c626d0477a056e53fc719937c78a23
RaihanMalay21 <raihanmalay14@gmail.com>
2024-12-24 09:34:35 -0800

    something wromg

--- router/router.go
+++ router/router.go
@@ -1,21 +1,15 @@
 package router
 
 import (
-	"html/template"
-
 	controllerAuth "github.com/RaihanMalay21/api-service-riors/controller/authentication"
 	controllerProducts "github.com/RaihanMalay21/api-service-riors/controller/products"
 	_ "github.com/RaihanMalay21/api-service-riors/docs"
 	"github.com/RaihanMalay21/api-service-riors/middlewares"
-	templateModule "github.com/RaihanMalay21/api-service-riors/template"
 	"github.com/labstack/echo/v4"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
 func InitRouter(e *echo.Echo, product controllerProducts.ProductController, category controllerProducts.CategoryController, auth controllerAuth.AuthenticationController) {
-	renderer := templateModule.ConstructorTemplate(template.Must(template.ParseGlob("././template/*.html")))
-	e.Renderer = renderer
-
 	e.Use(middlewares.CorsMiddlewares)
 	e.Use(middlewares.SetLimiterMiddleware)
 	e.GET("/swagger/*", echoSwagger.WrapHandler)

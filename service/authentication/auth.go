c1206ed5320b8728fbf555a614bbcee5525ec223
RaihanMalay21 <raihanmalay14@gmail.com>
2024-12-28 07:03:52 -0800

    mistake again

--- service/authentication/auth.go
+++ service/authentication/auth.go
@@ -296,16 +296,16 @@ func (ar *authenticationService) ChangePasswordAdmin(data *dto.ChangePassword, r
 		return http.StatusInternalServerError
 	}
 
-	if err := bcrypt.CompareHashAndPassword([]byte(Employee.Password), []byte(data.PasswordBefore)); err != nil {
-		switch err {
-		case bcrypt.ErrMismatchedHashAndPassword:
-			response["ErrorFields"] = map[string]string{"password": "Password anda salah"}
-			return http.StatusBadRequest
-		default:
-			response["error"] = err.Error()
-			return http.StatusInternalServerError
-		}
-	}
+	// if err := bcrypt.CompareHashAndPassword([]byte(Employee.Password), []byte(data.PasswordBefore)); err != nil {
+	// 	switch err {
+	// 	case bcrypt.ErrMismatchedHashAndPassword:
+	// 		response["ErrorFields"] = map[string]string{"password": "Password anda salah"}
+	// 		return http.StatusBadRequest
+	// 	default:
+	// 		response["error"] = err.Error()
+	// 		return http.StatusInternalServerError
+	// 	}
+	// }
 
 	if Employee.Password != data.PasswordBefore {
 		response["errorField"] = "password salah"
